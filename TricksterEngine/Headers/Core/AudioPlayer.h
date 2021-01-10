5f5a2dcffc450d4e0c2532c456374465c6706edd
Trickje <rick.pijpers@hotmail.com>
2021-01-10 21:44:22 +0100

    Copyright Notice where applicable

--- TricksterEngine/Headers/Core/AudioPlayer.h
+++ TricksterEngine/Headers/Core/AudioPlayer.h
@@ -3,7 +3,23 @@
 //Since this will just take care of placing audio, and playing it is handled by FMOD
 //For projects that make 200,000$ there has to be money paid to the makers of FMOD.
 
+/*
+================================================================================
+		Copyright 2021 Rick Pijpers
 
+		Licensed under the Apache License, Version 2.0 (the "License");
+		you may not use this file except in compliance with the License.
+		You may obtain a copy of the License at
+
+			http://www.apache.org/licenses/LICENSE-2.0
+
+		Unless required by applicable law or agreed to in writing, software
+		distributed under the License is distributed on an "AS IS" BASIS,
+		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+		See the License for the specific language governing permissions and
+		limitations under the License.
+=================================================================================
+ */
 
 namespace Trickster {
 
--- TricksterEngine/resource.h
+++ TricksterEngine/resource.h
@@ -2,7 +2,6 @@
 // Microsoft Visual C++ generated include file.
 // Used by TricksterEngine.rc
 //
-#define IDI_ICON1                       101
 
 // Next default values for new objects
 // 
